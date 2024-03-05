f2e74e78925d6fee403cb5ea5c9ce7008debc477
kauan-cancelier <kauanmellocancelier2005@gmail.com>
2024-03-05 11:45:21 -0300

    Refatoração da tela de login para utilização do service e dos novos componentes.

--- src/views/pages/login/Login.jsx
+++ src/views/pages/login/Login.jsx
@@ -1,51 +1,33 @@
-import React, { useState } from 'react'
-import { Form, FormInput, Message } from 'semantic-ui-react'
-import PrimaryButton from '../../components/PrimaryButton'
-import api from '../../../service/Api'
-import { Navigate } from 'react-router-dom'
+import React, { useMemo, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import LoginService from '../../../service/LoginService'
+import GenericForm from '../../components/layouts/Form'
 
 function Login() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: '',
+    })
     const [errorMessage, setErrorMessage] = useState('')
-    const [redirectToHome, setRedirectToHome] = useState(false)
+
+    const navigate = useNavigate()
+    const loginService = useMemo(() => new LoginService({ navigate, setErrorMessage }), [navigate])
 
     const login = async () => {
-        api.post('/login', {
-            email: email,
-            password: password,
-        }).then(response => {
-            if (response.status === 200) {
-                setRedirectToHome(true)
-            }
-        }).catch(error => {
-            if (error.response && error.response.status === 400) {
-                setErrorMessage(error.response.data.error)
-            } else {
-                console.log(error)
-            }
-        })
-    }
-
-    if (redirectToHome) {
-        return <Navigate to="/" state={{ message: `Bem vindo!` }} replace />
+        loginService.login(credentials)
     }
 
     return (
         <>
-            {errorMessage && <div><Message color='red'>{errorMessage}</Message><br /></div>}
             <h1>Login</h1>
-            <Form>
-                <FormInput label='email' placeholder='example@gmail.com' onChange={
-                    (e) => setEmail(e.target.value)
-                } />
-
-                <FormInput label='Senha' type='password' onChange={
-                    (e) => setPassword(e.target.value)
-                } />
-
-                <PrimaryButton label="Logar" onClick={login} />
-            </Form>
+            <GenericForm
+                data={credentials}
+                setData={setCredentials}
+                onClick={login}
+                buttonLabel='Entrar'
+                errorMessage={errorMessage}
+            />
         </>
     )
 }
