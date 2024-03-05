efaad5c8f10aa08e7fbf4856f839830b281f9859
kauan-cancelier <kauanmellocancelier2005@gmail.com>
2024-03-05 11:46:24 -0300

    Criado componente filter name.

---
+++ src/views/components/Filter.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import { Button, Form, FormInput, Modal } from "semantic-ui-react"
+
+function NameFilter({ onFilter }) {
+    const [modalState, setModalState] = useState(false)
+    const [name, setName] = useState("")
+
+    const sendFilter = () => {
+        setModalState(false)
+        onFilter(name)
+    }
+
+    return (
+        <>
+            <Button onClick={() => setModalState(true)} primary size="small">Filtro</Button>
+            <Modal
+                size={"small"}
+                open={modalState}
+                onClose={() => setModalState(false)}
+            >
+                <Modal.Header>Filtro</Modal.Header>
+                <Modal.Content>
+                    <Form>
+                        <FormInput
+                            label={"Nome"}
+                            name={"name"}
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </Form>
+                </Modal.Content>
+
+                <Modal.Actions>
+                    <Button positive onClick={() => sendFilter()}>
+                        Filtrar
+                    </Button>
+                </Modal.Actions>
+            </Modal>
+        </>
+    )
+}
+
+export default NameFilter
